d5215d3da9896cb9871d7a14612dbd97702c6120
Thiago Henrique <contato@thiagohenrique.com.br>
2021-01-30 23:50:43 -0300

    tests: a little bit change in names and function return

--- test/internal/vault/vault_test.go
+++ test/internal/vault/vault_test.go
@@ -6,13 +6,12 @@ import (
 	"github.com/junosql/server/internal/vault"
 )
 
-func TestVaultPackage(t *testing.T) {
-	thisVault := vault.NewVault(vault.HashiCorpBackend)
+func TestNewVault(t *testing.T) {
+	thisVault, _ := vault.NewVault(vault.HashiCorpBackend)
 
 	t.Run("should set type according argument backendType", func(t *testing.T) {
 		if thisVault.Type != vault.HashiCorpBackend {
 			t.Errorf("expected thisVault.Type go be %T but it was %T", vault.HashiCorpBackend, thisVault.Type)
 		}
 	})
-
 }
