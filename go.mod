4e0bc2d8ebb3a40238fc2e3d63f77a28a16f5cbb
Thiago Henrique <contato@thiagohenrique.com.br>
2021-01-31 00:55:15 -0300

    tests(vault): create tests for Retrieve and Save

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/junosql/server
 go 1.15
 
 require github.com/hashicorp/vault/api v1.0.4
+
+require (
+	github.com/satori/go.uuid v1.2.0
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
--- test/internal/vault/vault_test.go
+++ test/internal/vault/vault_test.go
@@ -1,9 +1,12 @@
 package vault__test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/junosql/server/internal/vault"
+
+	uuid "github.com/satori/go.uuid"
 )
 
 func TestNewVault(t *testing.T) {
@@ -23,6 +26,69 @@ func TestNewVault(t *testing.T) {
 
 			default:
 				t.Errorf("expected thisVault.Client go be %T but it was %T", vault.InMemoryClient{}, thisVault.Client)
+			}
+		})
+	})
+}
+
+func TestSave(t *testing.T) {
+	t.Run("when type is vault.InMemoryBackend", func(t *testing.T) {
+		thisVault, _ := vault.NewVault(vault.InMemoryBackend)
+
+		t.Run("shouldn't return an error", func(t *testing.T) {
+			if err := thisVault.Save("key", "value"); err != nil {
+				t.Errorf("expected no error, but got %s", err)
+			}
+		})
+
+		t.Run("should store the value inside global map", func(t *testing.T) {
+			key := fmt.Sprintf("%s", uuid.NewV4())
+			val := fmt.Sprintf("%s", uuid.NewV4())
+
+			thisVault.Save(key, val)
+
+			if vault.InMemoryValues[key] != val {
+				t.Errorf("expected '%s', but got '%s'", val, vault.InMemoryValues[key])
+			}
+		})
+	})
+}
+
+func TestRetrieve(t *testing.T) {
+	t.Run("when type is vault.InMemoryBackend", func(t *testing.T) {
+		thisVault, _ := vault.NewVault(vault.InMemoryBackend)
+
+		t.Run("when key exists should return it's value with no errors", func(t *testing.T) {
+			key := fmt.Sprintf("%s", uuid.NewV4())
+			val := fmt.Sprintf("%s", uuid.NewV4())
+
+			vault.InMemoryValues[key] = val
+
+			actualVal, actualErr := thisVault.Retrieve(key)
+
+			if actualErr != nil {
+				t.Errorf("expected no error, but got '%s'", actualErr)
+			}
+
+			if actualVal != val {
+				t.Errorf("expected '%s', but got '%s'", val, actualVal)
+			}
+		})
+
+		t.Run("when key doesn't exists should return error", func(t *testing.T) {
+			inexistentKey := "inexistentKey"
+
+			expectVal := ""
+			expectedErr := fmt.Errorf("The key %s doesn't exists", inexistentKey)
+
+			actualVal, actualErr := thisVault.Retrieve(inexistentKey)
+
+			if fmt.Sprintf("%s", actualErr) != fmt.Sprintf("%s", expectedErr) {
+				t.Errorf("expected error '%s' but got '%s'", expectedErr, actualErr)
+			}
+
+			if actualVal != expectVal {
+				t.Errorf("expected empty value but got '%s'", actualVal)
 			}
 		})
 	})
