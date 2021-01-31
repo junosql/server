4a3ec567e1fef7fa25243c868e35303665050ba8
Thiago Henrique <contato@thiagohenrique.com.br>
2021-01-31 00:01:49 -0300

    tests: adjust the tests for vault.NewVault

--- test/internal/vault/vault_test.go
+++ test/internal/vault/vault_test.go
@@ -7,11 +7,23 @@ import (
 )
 
 func TestNewVault(t *testing.T) {
-	thisVault, _ := vault.NewVault(vault.HashiCorpBackend)
+	t.Run("when type is vault.InMemoryBackend", func(t *testing.T) {
+		thisVault, _ := vault.NewVault(vault.InMemoryBackend)
 
-	t.Run("should set type according argument backendType", func(t *testing.T) {
-		if thisVault.Type != vault.HashiCorpBackend {
-			t.Errorf("expected thisVault.Type go be %T but it was %T", vault.HashiCorpBackend, thisVault.Type)
-		}
+		t.Run("should set Type according backendType", func(t *testing.T) {
+			if thisVault.Type != vault.InMemoryBackend {
+				t.Errorf("expected thisVault.Type go be %+v but it was %+v", vault.HashiCorpBackend, thisVault.Type)
+			}
+		})
+
+		t.Run("should set Client according backendType", func(t *testing.T) {
+			switch thisVault.Client.(type) {
+			case vault.InMemoryClient:
+				return
+
+			default:
+				t.Errorf("expected thisVault.Client go be %T but it was %T", vault.InMemoryClient{}, thisVault.Client)
+			}
+		})
 	})
 }
